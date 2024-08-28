d6ed80df67906013b7edb29967a9b2c8e36e0ee6
harshavardhan85 <harsha876.xworkz@gmail.com>
2024-08-28 15:35:31 +0530

    hi

---
+++ Bulb.java
@@ -0,0 +1,18 @@
+package com.xworkz.harsha;
+
+public class Bulb {
+    public String name;
+
+
+    public Bulb()
+    {
+        super();
+        System.out.println("Constructor with zero parameter is created");
+    }
+
+    public void display()
+    {
+        System.out.println("the bulb name is:"+name);
+    }
+}
+
---
+++ SmartPhone.java
@@ -0,0 +1,23 @@
+package com.xworkz.harsha;
+
+public class SmartPhone {
+
+
+        public String name;
+        public double cost;
+        public int quantity;
+        public String storage;
+
+        public SmartPhone()
+        {
+            super();
+        }
+        public void details(String name,double cost,int quantity,String storage)
+        {
+            System.out.println("the phone name is :"+name);
+            System.out.println("the cost is:"+cost);
+            System.out.println("Number of quantity :"+quantity);
+            System.out.println("phone storage is :"+storage);
+        }
+}
+
